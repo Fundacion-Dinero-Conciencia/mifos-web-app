8054abb5aa1829ce1ab7bceaa1beecf3e508c3ba
Jacobo Vargas <jacobo.vargas@cofincafe.com>
2025-03-25 11:29:33 -0500

    Fix error undefined un view to create savings account

--- src/app/savings/savings-account-stepper/savings-account-charges-step/savings-account-charges-step.component.ts
+++ src/app/savings/savings-account-stepper/savings-account-charges-step/savings-account-charges-step.component.ts
@@ -227,11 +227,14 @@ export class SavingsAccountChargesStepComponent implements OnInit, OnChanges {
     return { charges: this.chargesDataSource };
   }
 
+  // get selectedClientMembers() {
+  //   return { selectedMembers: this.activeClientMembers.filter((item: any) => item.selected) };
+  // }
+
   get selectedClientMembers() {
-    return {
-      selectedMembers: this.activeClientMembers ? this.activeClientMembers.filter((item: any) => item.selected) : []
-    };
+    return { selectedMembers: (this.activeClientMembers || []).filter((item: any) => item.selected) };
   }
+  
 
   /** Toggle all checks */
   toggleSelects() {
