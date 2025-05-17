83579a22fdca2f9cdcc13ce68200e3cb6cbcf854
edxmorales <81397998+edxmorales@users.noreply.github.com>
2025-05-17 13:49:09 -0500

    Update clients.service.ts

--- src/app/clients/clients.service.ts
+++ src/app/clients/clients.service.ts
@@ -317,7 +317,9 @@ export class ClientsService {
     };
     
     const httpParams = new HttpParams().set('command', command);
-    return this.http.post(`/clients/${clientId}`, data, { params: httpParams });
+    /**return this.http.post(`/clients/${clientId}`, data, { params: httpParams }); */
+    return this.http.post(`/clients/${clientId}`, enrichedData, { params: httpParams });
+
   }
 
   getClientCommandTemplate(command: string): Observable<any> {
