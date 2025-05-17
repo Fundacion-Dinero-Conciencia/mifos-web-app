7efc156ca2315801642f2b6c28e8d05b7d693c19
edxmorales <81397998+edxmorales@users.noreply.github.com>
2025-05-17 14:09:05 -0500

    Update clients.service.ts
    Ajuste Clients Service para leer ENV

--- src/app/clients/clients.service.ts
+++ src/app/clients/clients.service.ts
@@ -1,6 +1,7 @@
 /** Angular Imports */
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from 'environments/environment';
 
 /** rxjs Imports */
 import { Observable } from 'rxjs';
@@ -304,10 +305,19 @@ export class ClientsService {
     return this.http.put(`/client/${clientId}/addresses?type=${addressTypeId}`, addressData);
   }
 
-  executeClientCommand(clientId: string, command: string, data: any): Observable<any> {
-    const httpParams = new HttpParams().set('command', command);
-    return this.http.post(`/clients/${clientId}`, data, { params: httpParams });
-  }
+ executeClientCommand(clientId: string, command: string, data: any): Observable<any> {
+  const locale = window['env']?.['defaultLanguage'] || 'es';
+  const dateFormat = window['env']?.['defaultDateFormat'] || 'dd MMMM yyyy';
+
+  const enrichedData = {
+    ...data,
+    locale,
+    dateFormat
+  };
+
+  const httpParams = new HttpParams().set('command', command);
+  return this.http.post(`/clients/${clientId}`, enrichedData, { params: httpParams });
+ }
 
   getClientCommandTemplate(command: string): Observable<any> {
     const httpParams = new HttpParams().set('commandParam', command);
