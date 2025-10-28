f719d8db5304192b9a0c30ec046303aa656ef385
Sebastian Poveda <sebastian.poveda@ginkofs.com>
2025-10-28 10:00:28 -0500

    fix:groupid for download

--- src/environments/.env.ts
+++ src/environments/.env.ts
@@ -2,8 +2,8 @@
 /* tslint:disable */
 export default {
   'mifos_x': {
-    'version': '251022',
-    'hash': '45b73ef3'
+    'version': '251023',
+    'hash': 'fa4adf80'
   },
   'allow_switching_backend_instance': true
 };
