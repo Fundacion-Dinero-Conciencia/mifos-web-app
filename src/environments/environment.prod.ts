edca9f3c1216a5010c3af3643d207f53477186a1
Sebastian Poveda <48773112+kubson991@users.noreply.github.com>
2026-01-21 14:07:57 -0500

    Fix formatting of GOOGLE_MAPS_API_KEY in environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -61,9 +61,10 @@ export const environment = {
 
   minPasswordLength: loadedEnv['minPasswordLength'] || 12,
   baseUrlProject: 'https://investments-backend-dev.up.railway.app/project-details/',
-  GOOGLE_MAPS_API_KEY: 'AIzaSyBIuTaxrgjb1iYJwzGCcjdWZUfqpxW3T_k'
-};
+  GOOGLE_MAPS_API_KEY: 'AIzaSyBIuTaxrgjb1iYJwzGCcjdWZUfqpxW3T_k',
   amazonBucketUrl: 'https://bucketfinedev.s3.amazonaws.com/'
+  
+};
 
 // Server URL
 environment.serverUrl = `${environment.baseApiUrl}${environment.apiProvider}${environment.apiVersion}`;
