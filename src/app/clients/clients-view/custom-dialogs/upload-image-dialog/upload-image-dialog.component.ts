094b52ff9a8e1f4e848dd51411b6b60941228b05
Sebastian Poveda <sebastian.poveda@ginkofs.com>
2026-02-19 10:39:40 -0500

    upload multiple images

--- src/app/clients/clients-view/custom-dialogs/upload-image-dialog/upload-image-dialog.component.ts
+++ src/app/clients/clients-view/custom-dialogs/upload-image-dialog/upload-image-dialog.component.ts
@@ -1,6 +1,8 @@
 /** Angular Imports */
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Inject, OnInit } from '@angular/core';
 
 /**
  * Upload image dialog component.
@@ -10,22 +12,36 @@ import { MatDialogRef } from '@angular/material/dialog';
   templateUrl: './upload-image-dialog.component.html',
   styleUrls: ['./upload-image-dialog.component.scss']
 })
-export class UploadImageDialogComponent {
+export class UploadImageDialogComponent implements OnInit {
   /** Client Image */
-  image: File;
+  image: File | File[] | null = null;
 
   /**
    * @param {MatDialogRef} dialogRef Component reference to dialog.
    */
-  constructor(public dialogRef: MatDialogRef<UploadImageDialogComponent>) {}
+  constructor(
+    public dialogRef: MatDialogRef<UploadImageDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
 
   /**
    * Sets file form control value.
    * @param {any} $event file change event.
    */
-  onFileSelect($event: any) {
-    if ($event.target.files.length > 0) {
-      this.image = $event.target.files[0];
+
+  ngOnInit() {
+    console.log(this.data);
+  }
+
+  onFileSelect(event: any) {
+    const files: FileList = event.target.files;
+
+    if (!files || files.length === 0) return;
+
+    if (this.data.multiple) {
+      this.image = Array.from(files);
+    } else {
+      this.image = files[0];
     }
   }
 }
